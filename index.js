31c27db9d55a962a01ebeffb4999c006c6a5bf91
ajeet. <Ajeetsingh97835@gmail.com>
2025-01-19 15:50:40 +0530

    2

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const menuIcon = document.querySelector('#menuImg');
-const navLinks = document.querySelector('.nav-links');
+// const menuIcon = document.querySelector('#menuImg');
+// const navLinks = document.querySelector('.nav-links');
 
-menuIcon.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
+// menuIcon.addEventListener('click', () => {
+//     navLinks.classList.toggle('show');
+// });
+
+const account = document.querySelector('#account');
+const form = document.querySelector('.box');
+
+
+const defaultContent = form.innerHTML;
+
+account.addEventListener('click', () => {
+    form.innerHTML = 'Profile';
 });
 
-const form = document.querySelector('form');
-const notes = [];
+const notes = document.querySelector('#notes');
+notes.addEventListener('click', () => {
+    form.innerHTML = 'Notes and stuff';
+});
+
+const home = document.querySelector('#home');
+home.addEventListener('click', () => {
+    form.innerHTML = defaultContent;
+});
+
+
